325d2531fc788e7d25e513c9b228690b551d2e99
Thomas Ramel <thomas@thomasramel.com>
2023-02-15 12:03:02 +0100

    OCT-220: rework catalog mapping storage usage

--- components/catalogs/back/src/Application/Persistence/ProductMappingSchema/GetProductMappingSchemaQueryInterface.php
+++ components/catalogs/back/src/Application/Persistence/ProductMappingSchema/GetProductMappingSchemaQueryInterface.php
@@ -7,13 +7,15 @@ namespace Akeneo\Catalogs\Application\Persistence\ProductMappingSchema;
 /**
  * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *
+ * @phpstan-type ProductMappingSchema array{
+ *      properties: array<array-key, mixed>
+ * }
  */
 interface GetProductMappingSchemaQueryInterface
 {
     /**
-     * @return array{
-     *      properties: array<array-key, mixed>
-     * }
+     * @return ProductMappingSchema
      */
     public function execute(string $catalogId): array;
 }
